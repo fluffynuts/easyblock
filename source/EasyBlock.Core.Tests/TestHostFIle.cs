a23ae8c5a86e23a60de9b8cf9f40ec0c7cfb9167
Davyd McColl <davydm@gmail.com>
2016-02-25 21:26:57 +0200

    * remove dup

--- source/EasyBlock.Core.Tests/TestHostFIle.cs
+++ source/EasyBlock.Core.Tests/TestHostFIle.cs
@@ -150,18 +150,4 @@ namespace EasyBlock.Core.Tests
         }
     }
 
-    public static class TestExtensionsForTextFileReader
-    {
-        public static void SetData(this ITextFileReader reader, IEnumerable<string> lines)
-        {
-            var queue = new Queue<string>(lines);
-            reader.ReadLine().Returns(ci =>
-            {
-                if (!queue.Any())
-                    return null;
-                return queue.Dequeue();
-            });
-        }
-
-    }
 }
