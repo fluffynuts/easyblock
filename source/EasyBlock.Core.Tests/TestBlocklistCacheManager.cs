693377d4c620f737fe06f98d321e6951330b60c6
Davyd McColl <davyd.mccoll@chillisoft.co.za>
2016-03-02 20:05:56 +0200

    * update PB deps * start on cache manager

---
+++ source/EasyBlock.Core.Tests/TestBlocklistCacheManager.cs
@@ -0,0 +1,91 @@
+﻿using System;
+using NUnit.Framework;
+using PeanutButter.INIFile;
+using PeanutButter.RandomGenerators;
+using PeanutButter.TestUtils.Generic;
+
+namespace EasyBlock.Core.Tests
+{
+    [TestFixture]
+    public class TestBlocklistCacheManager
+    {
+        [Test]
+        public void Type_ShouldImplement_IBlocklistCacheManager()
+        {
+            //---------------Set up test pack-------------------
+            var sut = typeof(BlocklistCacheManager);
+
+            //---------------Assert Precondition----------------
+
+            //---------------Execute Test ----------------------
+            sut.ShouldImplement<IBlocklistCacheManager>();
+
+            //---------------Test Result -----------------------
+        }
+
+        [Test]
+        public void Construct_GivenNullCacheFilenameGenerator_ShouldThrowANE()
+        {
+            //---------------Set up test pack-------------------
+
+            //---------------Assert Precondition----------------
+
+            //---------------Execute Test ----------------------
+            Assert.Throws<ArgumentNullException>(() => new BlocklistCacheManager((ICacheFilenameGenerator)null));
+
+            //---------------Test Result -----------------------
+        }
+
+
+        [Test]
+        [Ignore("WIP: requires cache filename generator")]
+        public void Cache_GivenSourceAndData_ShouldWriteDataToCacheLocation()
+        {
+            //---------------Set up test pack-------------------
+
+            //---------------Assert Precondition----------------
+
+            //---------------Execute Test ----------------------
+
+            //---------------Test Result -----------------------
+            Assert.Fail("Test Not Yet Implemented");
+        }
+    }
+
+    [TestFixture]
+    public class TestCacheFilenameGenerator
+    {
+        [Test]
+        public void Construct_GivenNullINIFile_ShouldThrowANE()
+        {
+            //---------------Set up test pack-------------------
+
+            //---------------Assert Precondition----------------
+
+            //---------------Execute Test ----------------------
+            Assert.Throws<ArgumentNullException>(() => new CacheFilenameGenerator((IINIFile)null));
+
+            //---------------Test Result -----------------------
+        }
+
+        [Test]
+        [Ignore("WIP")]
+        public void GenerateFor_ShouldReturnPathUnderConfiguredCachePath()
+        {
+            //---------------Set up test pack-------------------
+            //var expected = RandomValueGen.GetRandomFileName
+            //var iniFile = IniBuilder.Create()
+            //                    .WithCacheLocation(
+            //var sut = Create(
+
+            //---------------Assert Precondition----------------
+
+            //---------------Execute Test ----------------------
+
+            //---------------Test Result -----------------------
+            Assert.Fail("Test Not Yet Implemented");
+        }
+
+
+    }
+}
---
+++ source/EasyBlock.Core/BlocklistCacheManager.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using PeanutButter.INIFile;
+
+namespace EasyBlock.Core
+{
+    public interface IBlocklistCacheManager
+    {
+        void Cache(string source, byte[] data);
+        byte[] Find(string source);
+    }
+
+    public class BlocklistCacheManager: IBlocklistCacheManager
+    {
+        private readonly ICacheFilenameGenerator _cacheFilenameGenerator;
+
+        public BlocklistCacheManager(ICacheFilenameGenerator cacheFilenameGenerator)
+        {
+            if (cacheFilenameGenerator == null) throw new ArgumentNullException(nameof(cacheFilenameGenerator));
+            _cacheFilenameGenerator = cacheFilenameGenerator;
+        }
+
+        public void Cache(string source, byte[] data)
+        {
+            throw new NotImplementedException();
+        }
+
+        public byte[] Find(string source)
+        {
+            throw new NotImplementedException();
+        }
+    }
+
+    public interface ICacheFilenameGenerator
+    {
+        string GenerateFor(string sourceUrl);
+    }
+
+    public class CacheFilenameGenerator
+    {
+        private readonly IINIFile _iniFile;
+
+        public CacheFilenameGenerator(IINIFile iniFile)
+        {
+            if (iniFile == null) throw new ArgumentNullException(nameof(iniFile));
+            _iniFile = iniFile;
+        }
+    }
+}
