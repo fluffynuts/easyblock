8f9ca8d2cb9ec381f7a2c213b517f666c4cd4f1c
Davyd McColl <davyd.mccoll@chillisoft.co.za>
2016-03-18 09:47:24 +0200

    * prove that the service entry point runs the Shell

---
+++ source/EasyBlock.Win32Service.Tests/TestProgram.cs
@@ -0,0 +1,28 @@
+﻿using System.Linq;
+using NUnit.Framework;
+using PeanutButter.ServiceShell;
+using static PeanutButter.RandomGenerators.RandomValueGen;
+
+namespace EasyBlock.Win32Service.Tests
+{
+    [TestFixture]
+    public class TestProgram
+    {
+        [Test]
+        public void Main_ShouldRunEasyBlockServiceMainWithProvidedArguments()
+        {
+            //---------------Set up test pack-------------------
+            Shell.StartTesting();
+            var args = GetRandomCollection<string>(2,4).ToArray();
+
+            //---------------Assert Precondition----------------
+
+            //---------------Execute Test ----------------------
+            Program.Main(args);
+
+            //---------------Test Result -----------------------
+            Shell.ShouldHaveRunMainFor<EasyBlockService>(args);
+        }
+
+    }
+}
--- source/EasyBlock.Win32Service/Program.cs
+++ source/EasyBlock.Win32Service/Program.cs
@@ -1,15 +1,12 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using PeanutButter.ServiceShell;
 
 namespace EasyBlock.Win32Service
 {
-    public class Program
+    public static class Program
     {
-        static void Main(string[] args)
+        public static void Main(string[] args)
         {
+            Shell.RunMain<EasyBlockService>(args);
         }
     }
 }
